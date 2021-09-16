4835da6df8d08a1e205abdff0bd37243d87358e1
witomin <56428922+witomin@users.noreply.github.com>
2021-09-16 12:46:35 +0300

    Update jquery.IpInput.js

--- jquery.IpInput.js
+++ jquery.IpInput.js
@@ -39,12 +39,17 @@
                     }
                 }
                 // одиночный адрес
-                else
+                else {
+                    if (item == '') {
+                        status = true;
+                    }
+                    else
                     //проверяем корректность
                     if (!chek_valid_ip(item)) {
                         status = false;
                         return;
                     }
+                }
                 status = true;
             });
 
@@ -58,43 +63,43 @@
                 Element.data({ Addreses: null });
                 Element.data({ Status: false });
             }
+            
         }
         return this;
-
     };
+    // валидация ip
+    function chek_valid_ip(ip) {
+        if (ip.match('^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$') && ip.split('.').length == 4)
+            return true;
+        else
+            return false;
+    }
+    // валидация диапазона ip
+    function chek_valid_diapazon(diapazon) {
+        var addreses = diapazon.split('-');
+        if (addreses.length != 2) {
+            return false;
+        }
+        if (!chek_valid_ip(addreses[0]) || !chek_valid_ip(addreses[1])) {
+            return false;
+        }
+        var begin = addreses[0].split('.');
+        var end = addreses[1].split('.');
+        X = begin[0] * Math.pow(256, 3) + begin[1] * Math.pow(256, 2) + begin[2] * Math.pow(256, 1) + begin[3] * Math.pow(256, 0);
+        Y = end[0] * Math.pow(256, 3) + end[1] * Math.pow(256, 2) + end[2] * Math.pow(256, 1) + end[3] * Math.pow(256, 0);
+        if (X < Y)
+            return true;
+        else
+            return false;
+    }
+    // валидация маски
+    function chek_valid_mask(mask) {
+
+        var addreses = mask.split('/');
+        if (addreses.length != 2) return false;
+        if (!chek_valid_ip(addreses[0])) return false;
+        if (!addreses[1].match('^[2-9]$|^[1-2][0-9]$|^3[0-2]$')) return false;
+        return true;
+    }
 })(jQuery);
 
-// валидация ip
-function chek_valid_ip(ip) {
-    if (ip.match('^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$') && ip.split('.').length == 4)
-        return true;
-    else
-        return false;
-}
-// валидация диапазона ip
-function chek_valid_diapazon(diapazon) {
-    var addreses = diapazon.split('-');
-    if (addreses.length != 2) {
-        return false;
-    }
-    if (!chek_valid_ip(addreses[0]) || !chek_valid_ip(addreses[1])) {
-        return false;
-    }
-    var begin = addreses[0].split('.');
-    var end = addreses[1].split('.');
-    X = begin[0] * Math.pow(256, 3) + begin[1] * Math.pow(256, 2) + begin[2] * Math.pow(256, 1) + begin[3] * Math.pow(256, 0);
-    Y = end[0] * Math.pow(256, 3) + end[1] * Math.pow(256, 2) + end[2] * Math.pow(256, 1) + end[3] * Math.pow(256, 0);
-    if (X < Y)
-        return true;
-    else
-        return false;
-}
-// валидация маски
-function chek_valid_mask(mask) {
-
-    var addreses = mask.split('/');
-    if (addreses.length != 2) return false;
-    if (!chek_valid_ip(addreses[0])) return false;
-    if (!addreses[1].match('^[2-9]$|^[1-2][0-9]$|^3[0-2]$')) return false;
-    return true;
-}
